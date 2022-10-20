54679da1239e531908d8b0783a9a1ef4a8275c71
abdejohan <abdejohan@gmail.com>
2022-10-20 21:55:07 +0200

    Add client specific values to talkjs component

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,21 +1,16 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "../context/Auth";
 import * as TalkRn from "@talkjs/expo";
 import { useIsFocused } from "@react-navigation/native";
 import NotificationsContext from "../context/Notifications";
 import Constants from "expo-constants";
-import { View } from "react-native";
-import { ActivityIndicator, useTheme } from "react-native-paper";
-import { Headline } from "../typography";
 
 const ChatScreen: React.FC = () => {
 	const { user } = useContext(AuthContext);
-	const [displayChat, setDisplayChat] = useState(false);
-	const { colors } = useTheme();
 	const { setChatBadgeCount } = useContext(NotificationsContext);
 	const isFocused = useIsFocused();
 
-	const me: TalkRn.User = {
+	const userChatProfile: TalkRn.User = {
 		id: "FL_" + user!.id.toString(),
 		photoUrl: user?.avatar,
 		name: user?.name,
@@ -28,42 +23,17 @@ const ChatScreen: React.FC = () => {
 		}
 	}, [isFocused]);
 
-	useEffect(() => {
-		setTimeout(() => {
-			setDisplayChat(true);
-		}, 2000);
-	}, []);
-
-	const other: TalkRn.User = { id: "FL_1" };
-	const conversationBuilder = TalkRn.getConversationBuilder(TalkRn.oneOnOneId(me, other));
-	conversationBuilder.setParticipant(me);
-	conversationBuilder.setParticipant(other);
+	const coachChatProfile: TalkRn.User = { id: "FL_1" };
+	const conversationBuilder = TalkRn.getConversationBuilder(
+		TalkRn.oneOnOneId(userChatProfile, coachChatProfile)
+	);
+	conversationBuilder.setParticipant(userChatProfile);
+	conversationBuilder.setParticipant(coachChatProfile);
 
 	return (
-		<View style={{ flex: 1, backgroundColor: colors.background }}>
-			{!displayChat && (
-				<View
-					style={{
-						height: "100%",
-						backgroundColor: colors.background,
-						position: "absolute",
-						width: "100%",
-						justifyContent: "center",
-						alignItems: "center",
-						zIndex: 2,
-					}}>
-					<Headline style={{ marginBottom: 50 }}>Laddar chatten..</Headline>
-					<ActivityIndicator animating={true} size={50} color={colors.text} />
-				</View>
-			)}
-			<TalkRn.Session appId={Constants?.manifest?.extra?.chatAppID} me={me}>
-				<TalkRn.Chatbox
-					showChatHeader={false}
-					highlightedWords={["Test"]} // only works with TalkJS growth plan
-					conversationBuilder={conversationBuilder}
-				/>
-			</TalkRn.Session>
-		</View>
+		<TalkRn.Session appId={Constants?.manifest?.extra?.chatAppID} me={userChatProfile}>
+			<TalkRn.Chatbox showChatHeader={false} conversationBuilder={conversationBuilder} />
+		</TalkRn.Session>
 	);
 };
 
