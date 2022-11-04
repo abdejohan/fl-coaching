3063f08e834cc9f1d00d3e226613c111b43fd8e7
abdejohan <abdejohan@gmail.com>
2022-11-04 21:20:09 +0100

    Increment version number

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default {
 		owner: "coach-apps",
 		name: "FL Coaching",
 		slug: "fl-coaching",
-		version: "1.0.2",
+		version: "1.0.3",
 		orientation: "portrait",
 		icon: "./assets/images/icon.png",
 		scheme: "myapp",
