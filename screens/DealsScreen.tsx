1007f2c4d49e612b3271b8a93aea3af9c3427917
abdejohan <abdejohan@gmail.com>
2022-11-06 00:16:39 +0100

    Style DealScreen.tsx

--- screens/DealsScreen.tsx
+++ screens/DealsScreen.tsx
@@ -1,8 +1,8 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { Linking, ScrollView } from "react-native";
+import { Linking, ScrollView, View } from "react-native";
 import { useTheme, Card } from "react-native-paper";
 import Button from "../components/common/Button";
-import { Text } from "../typography";
+import { Headline, Text } from "../typography";
 
 const deals = [
 	{
@@ -12,7 +12,7 @@ const deals = [
 		url: "https://www.fantasticline.se/sv/",
 	},
 	{
-		title: "15% rabatt på massage Motala Fitness Center",
+		title: "15% rabatt på massage hos Motala Fitness Center",
 		description:
 			"Som klient hos oss har du 20% rabatt på alla våra massagebehandlingar på Motala Fitness Center. Rabatten dras av när du betalar på plats, uppvisa bara ditt inlogg i vår app i kassan.",
 		url: "https://motalafitnesscenter.se/",
@@ -30,24 +30,25 @@ const DealsScreen: React.FC = () => {
 				backgroundColor: colors.background,
 			}}>
 			{deals.map((deal, index) => (
-				<Card style={{ marginBottom: 20 }} key={index}>
-					<Card.Title
-						titleStyle={{ textDecorationLine: "underline" }}
-						leftStyle={{ marginRight: 0 }}
-						title={deal.title}
-						left={() => (
-							<MaterialIcons name='local-offer' size={30} color={colors.primary} />
-						)}
-					/>
-					{/**
-					 * 
-					<Card.Cover
-						source={{
-							uri: deal.image,
-						}}
-					/>
-						*/}
+				<Card style={{ marginBottom: 20, padding: 0 }} key={index}>
 					<Card.Content>
+						<View style={{ flexDirection: "row", marginBottom: 20 }}>
+							<MaterialIcons
+								name='local-offer'
+								size={20}
+								color={colors.primary}
+								style={{ top: 3 }}
+							/>
+							<Headline
+								style={{
+									marginLeft: 5,
+									fontSize: 20,
+									lineHeight: 22,
+									flexShrink: 1,
+								}}>
+								{deal.title}
+							</Headline>
+						</View>
 						<Text style={{ paddingVertical: 10, paddingHorizontal: 5 }}>
 							{deal.description}
 						</Text>
